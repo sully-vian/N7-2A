e5268dc1fddffef2db02c2f17eb4de8f4ff192df
hervyv <vianney.hervy@etu.inp-n7.fr>
2024-10-01 11:52:33 +0200

    PF TP3 terminé

--- PF/TP3/tp3.ml
+++ PF/TP3/tp3.ml
@@ -4,8 +4,8 @@
 (*** Code binaires de Gray ***)
 
 (* Fonction qui génère un code de Gray
- * Paramètre n : la taille du code
- * Resultat : le code sous forme de int list list
+ * Paramètre n : (int) la taille du code
+ * Résultat : (int list list) le code sous forme de int list list
  *)
 
 
@@ -29,29 +29,48 @@ let%test _ = gray_code 3 = [[0; 0; 0]; [0; 0; 1]; [0; 1; 1]; [0; 1; 0]; [1; 1; 0
   [1; 1; 1; 1]; [1; 1; 1; 0]; [1; 0; 1; 0]; [1; 0; 1; 1]; [1; 0; 0; 1];
   [1; 0; 0; 0]]
 
-(*
+
 (*** Combinaisons d'une liste ***)
 
-(* CONTRAT 
-TO DO
-*)
-let combinaison k l = failwith "TO DO"
+(* Fonction qui renvoie la liste de toutes les combinaisons possibles de k
+éléments dans l
+ * Paramètre k : (int) le nombre d'élémnts à prendre dans l
+ * Paramètre l : ('a list) la liste dont on veut k éléments
+ * Résultats : ('a list list) la liste de toutes les combinaisons possibles de k
+ * Pré : |l| >= k >= 0
+ * Post : - L'ordre initial des éléments est gardé dans chacune des combinaisons
+ *        - l' = combinaisons k l =>|l| = n! / ((n-k)! k!)
+ *)
+let rec combinaisons k l =
+    match k,l with
+    | 0,_ -> [[]]
+    | _,[] -> []
+    | _,h::t ->
+        let l1 = List.map (fun l' -> h::l') (combinaisons (k-1) t) in
+        let l2 = combinaisons k t in
+        l1@l2
 
 (* TESTS *)
-(* TO DO *)
-
+let%test _ = combinaisons 3 [1;2;3;4] = [[1;2;3]; [1;2;4]; [1;3;4]; [2;3;4]]
+let%test _ = combinaisons 1 ['a';'b';'c'] = [['a'];['b'];['c']]
+let%test _ = combinaisons 3 [1;2;3] = [[1;2;3]]
+let%test _ = combinaisons 0 [true;false;true;false;false] = [[]]
+let%test _ = combinaisons 12 [] = []
 
 
 (*** Permutations d'une liste ***)
 
-(* CONTRAT
-Fonction prend en paramètre un élément e et une liste l et qui insére e à toutes les possitions possibles dans l
-Pamaètre e : ('a) l'élément à insérer
-Paramètre l : ('a list) la liste initiale dans laquelle insérer e
-Reesultat : la liste des listes avec toutes les insertions possible de e dans l
-*)
+(* Fonction prend en paramètre un élément e et une liste l et qui insére e à toutes les possitions possibles dans l
+ * Pamaètre e : ('a) l'élément à insérer
+ * Paramètre l : ('a list) la liste initiale dans laquelle insérer e
+ * Résultat : la liste des listes avec toutes les insertions possible de e dans l
+ *)
 
-let rec insertion e l = failwith "TO DO"
+let rec insertion e l =
+    match l with
+    | [] -> [[e]]
+    | h::t -> let insert_t = insertion e t in
+    (e::l)::(List.map (fun l' -> h::l') insert_t)
 
 (* TESTS *)
 let%test _ = insertion 0 [1;2] = [[0;1;2];[1;0;2];[1;2;0]]
@@ -65,49 +84,54 @@ let%test _ = insertion 5 [12;54;0;3;78] =
  [['x'; 'a'; 'b'; 'c']; ['a'; 'x'; 'b'; 'c']; ['a'; 'b'; 'x'; 'c'];
   ['a'; 'b'; 'c'; 'x']]
 
+(* Fonction qui renvoie la liste des permutations d'une liste
+ * Paramètre l : ('a list) une liste
+ * Résultat : ('a list list) la liste des permutatiions de l (toutes différentes si les élements de l sont différents deux à deux
+ *)
 
-(* CONTRAT
-Fonction qui renvoie la liste des permutations d'une liste
-Paramètre l : une liste
-Résultat : la liste des permutatiions de l (toutes différentes si les élements de l sont différents deux à deux 
-*)
-
-let rec permutations l = failwith "TO DO"
+let rec permutations l =
+    match l with
+    | [] -> [[]]
+    | h::t -> List.flatten (List.map (insertion h) (permutations t))
 
 (* TESTS *)
-(*
 let l1 = permutations [1;2;3]
 let%test _ = List.length l1 = 6
-let%test _ = List.mem [1; 2; 3] l1 
-let%test _ = List.mem [2; 1; 3] l1 
-let%test _ = List.mem [2; 3; 1] l1 
-let%test _ = List.mem [1; 3; 2] l1 
-let%test _ = List.mem [3; 1; 2] l1 
-let%test _ = List.mem [3; 2; 1] l1 
+let%test _ = List.mem [1; 2; 3] l1
+let%test _ = List.mem [2; 1; 3] l1
+let%test _ = List.mem [2; 3; 1] l1
+let%test _ = List.mem [1; 3; 2] l1
+let%test _ = List.mem [3; 1; 2] l1
+let%test _ = List.mem [3; 2; 1] l1
 let%test _ = permutations [] =[[]]
 let l2 = permutations ['a';'b']
 let%test _ = List.length l2 = 2
-let%test _ = List.mem ['a';'b'] l2 
-let%test _ = List.mem ['b';'a'] l2 
-*)
-
+let%test _ = List.mem ['a';'b'] l2
+let%test _ = List.mem ['b';'a'] l2
 
 (*** Partition d'un entier ***)
 
 (* partition int -> int list
-Fonction qui calcule toutes les partitions possibles d'un entier n
-Paramètre n : un entier dont on veut calculer les partitions
-Préconditions : n >0
-Retour : les partitions de n
+ * Fonction qui calcule toutes les partitions possibles d'un entier n
+ * Paramètre n : un entier dont on veut calculer les partitions
+ * Préconditions : n >0
+ * Retour : les partitions de n
 *)
 
-let partition n = failwith "TO DO"
-
+let partitions n =
+    let rec aux t n =
+    if (t > n) then
+        []
+    else if (t = n) then
+        [[n]]
+    else
+        let lt = List.map (fun l' -> t::l') (aux t (n-t)) in
+        let l = aux (t+1) n in
+        lt@l
+    in aux 1 n
 
 (* TEST *)
-let%test _ = partition 1 = [[1]]
-let%test _ = partition 2 = [[1;1];[2]]
-let%test _ = partition 3 = [[1; 1; 1]; [1; 2]; [3]]
-let%test _ = partition 4 = [[1; 1; 1; 1]; [1; 1; 2]; [1; 3]; [2; 2]; [4]]
-
-*)
+let%test _ = partitions 1 = [[1]]
+let%test _ = partitions 2 = [[1;1];[2]]
+let%test _ = partitions 3 = [[1; 1; 1]; [1; 2]; [3]]
+let%test _ = partitions 4 = [[1; 1; 1; 1]; [1; 1; 2]; [1; 3]; [2; 2]; [4]]
