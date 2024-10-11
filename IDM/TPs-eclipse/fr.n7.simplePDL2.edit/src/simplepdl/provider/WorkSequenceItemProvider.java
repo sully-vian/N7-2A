26c42da3213fcdb30807aa1571dd0705dce9da74
hervyv <vianney.hervy@etu.inp-n7.fr>
2024-10-11 16:16:24 +0200

    IDM TP6 finished, mais je fais encore le PetriNetToDot

--- IDM/TPs-eclipse/fr.n7.simplePDL2.edit/src/simplepdl/provider/WorkSequenceItemProvider.java
+++ IDM/TPs-eclipse/fr.n7.simplePDL2.edit/src/simplepdl/provider/WorkSequenceItemProvider.java
@@ -138,19 +138,11 @@ public class WorkSequenceItemProvider extends ProcessElementItemProvider {
 	 */
 	@Override
 	public String getText(Object object) {
-//		WorkSequenceType labelValue = ((WorkSequence)object).getLinkType();
-//		String label = labelValue == null ? null : labelValue.toString();
-//		return label == null || label.length() == 0 ?
-//			getString("_UI_WorkSequence_type") :
-//			getString("_UI_WorkSequence_type") + " " + label;
-		WorkSequence ws = (WorkSequence) object;
-		WorkSequenceType labelValue = ws.getLinkType();
-		String label = "--" + (labelValue == null ? "?" : labelValue.toString()) + "-->";
-		String previous = ws.getPredecessor() == null ? "?" : ws.getPredecessor().getName();
-		String next = ws.getSuccessor() == null ? "?" : ws.getSuccessor().getName();
+		WorkSequenceType labelValue = ((WorkSequence)object).getLinkType();
+		String label = labelValue == null ? null : labelValue.toString();
 		return label == null || label.length() == 0 ?
-				getString("_UI_WorkSequence_type") :
-				getString("_UI_WorkSequence_type") + " " + previous + " " + label + " " + next;
+			getString("_UI_WorkSequence_type") :
+			getString("_UI_WorkSequence_type") + " " + label;
 	}
 
 
@@ -167,8 +159,6 @@ public class WorkSequenceItemProvider extends ProcessElementItemProvider {
 
 		switch (notification.getFeatureID(WorkSequence.class)) {
 			case SimplepdlPackage.WORK_SEQUENCE__LINK_TYPE:
-			case SimplepdlPackage.WORK_SEQUENCE__PREDECESSOR:
-			case SimplepdlPackage.WORK_SEQUENCE__SUCCESSOR:
 				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
 				return;
 		}
