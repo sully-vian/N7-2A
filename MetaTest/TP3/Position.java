cec8bc6a0ab472e9b39a2432cf59cba4ce2a246a
hervyv <vianney.hervy@etu.inp-n7.fr>
2024-11-22 17:51:15 +0100

    MetaTest-TP3: "fini" mais on s'n fout. PS: revenir voir pour les annotations Lombok

---
+++ MetaTest/TP3/Position.java
@@ -0,0 +1,15 @@
+/** Définir une position.  */
+public class Position {
+	public int x;
+	public int y;
+
+	public Position(int x, int y) {
+		this.x = x;
+		this.y = y;
+		System.out.println("...appel à Position(" + x + "," + y + ")" + " --> " + this);
+	}
+
+	@Override public String toString() {
+		return super.toString() + "(" + x + "," + y + ")";
+	}
+}
