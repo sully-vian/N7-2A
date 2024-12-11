e5a3de255dc92e8f326cc660354bfaf689ef3f2f
hervyv <vianney.hervy@etu.inp-n7.fr>
2024-12-11 09:54:20 +0100

    build script fix

--- HagiMule/scripts/build.sh
+++ HagiMule/scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Compile the java source files to the bin directory
-javac -d bin $(find src -name "*.java")
+javac -d bin -cp bin:lib/* $(find src -name "*.java")
 
 if [ $? -ne 0 ]; then
     echo -e "\e[31mBuild failed.\e[0m"
