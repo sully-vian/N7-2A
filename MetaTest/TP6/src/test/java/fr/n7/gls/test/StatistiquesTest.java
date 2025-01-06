886ef3c06565fbb96c790a986dd2991993971400
hervyv <vianney.hervy@etu.inp-n7.fr>
2025-01-06 18:50:13 +0100

    chépa, des trucs de MétaTest

---
+++ MetaTest/TP6/src/test/java/fr/n7/gls/test/StatistiquesTest.java
@@ -0,0 +1,32 @@
+/**
+ * StatistiquesTest, classe de test de Statistiques.
+ *
+ * @author	Xavier Crégut &lt;Prenom.Nom@enseeiht.fr&gt;
+ */
+
+package fr.n7.gls.test;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+public class StatistiquesTest {
+
+	@Test
+	public void testStatistiquesNominal() {
+		List<Integer> l = new ArrayList<>();
+		Collections.addAll(l, 1, 3, 11, 5, 7);
+		Statistiques.Resultat<Integer> r = new Statistiques().statistiques(l);
+		assertEquals(Integer.valueOf(1), r.min);
+		assertEquals(Integer.valueOf(11), r.max);
+		assertEquals(1, r.nbMin);
+		assertEquals(1, r.nbMax);
+	}
+
+	@Test
+	public void testStatistiques1(){
+
+	}
+
+}
