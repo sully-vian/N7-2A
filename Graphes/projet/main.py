6fc107ab1761fad50742d3206d1611c45783cf92
hervyv <vianney.hervy@etu.inp-n7.fr>
2025-01-10 18:05:18 +0100

    pas fini, ptet une erreur dans les composantes connexes

--- Graphes/projet/main.py
+++ Graphes/projet/main.py
@@ -1,4 +1,5 @@
 import csv
+import matplotlib as mp
 import matplotlib.pyplot as plt
 
 def readCSV(filename: str):
@@ -20,7 +21,6 @@ def plotPoints(points: list):
     ys = [point[1] for point in points]
     zs = [point[2] for point in points]
     ax.scatter(xs, ys, zs)
-    plt.show()
 
 def getAdjMatrix(points: list, reach:int):
     m = [[0 for i in range(len(points))] for i in range(len(points))]
@@ -68,7 +68,7 @@ def getAvgDegree(points:list, reach:int):
     degrees = getDegrees(points,reach)
     return sum(degrees) / len(degrees)
 
-def plotDegree(points:list, reach:int):
+def plotDegrees(points:list, reach:int):
     degrees = getDegrees(points, reach)
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
@@ -76,14 +76,137 @@ def plotDegree(points:list, reach:int):
     ys = [point[1] for point in points]
     zs = [point[2] for point in points]
     ax.scatter(xs, ys, zs, c=degrees, cmap='jet')
-    plt.show()
 
 def plotDegreeHist(points:list, reach:int):
     degrees = getDegrees(points, reach)
     n_bins = 10
+    plt.figure()
     plt.hist(degrees, bins=n_bins)
-    plt.show()
 
-points = readCSV("./topology_high.csv")
+def getLocalClusteringDegrees(points:list, reach:int):
+    n = len(points)
+    m = getAdjMatrix(points, reach)
+    d = getDegrees(points, reach)
+    numTriangles = [0 for i in range(len(points))]
+    triangleTot = 0
+    for i in range(n):
+        for j in range(i+1,n):
+            for k in range(j+1,n):
+                if (m[i][j] * m[j][k] * m[k][i]):
+                    triangleTot += 1
+                    numTriangles[i] += 1
+                    numTriangles[j] += 1
+                    numTriangles[k] += 1
+    res = [numTriangles[i] / (d[i]*(d[i]-1) / 2) if d[i] > 1 else 0 for i in range(n)]
+    return res
 
-plotDegreeHist(points, 60_000)
+def plotClusteringDegrees(points:list, reach:int):
+    clustDeg = getLocalClusteringDegrees(points, reach)
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    xs = [point[0] for point in points]
+    ys = [point[1] for point in points]
+    zs = [point[2] for point in points]
+    ax.scatter(xs, ys, zs, c=clustDeg, cmap='jet')
+
+def plotClusteringDegreeHist(points:list, reach:int):
+    clusteringDegrees = getLocalClusteringDegrees(points, reach)
+    n_bins = 10
+    plt.figure()
+    plt.hist(clusteringDegrees, bins=n_bins)
+
+def getCliques(points:list, reach:int):
+    m = getAdjMatrix(points, reach)
+    cliques = [[0]]
+    for i in range(1,len(points)):
+        added = False
+        for clique in cliques:
+            # vérifier si i appartient à la clique
+            inClique = True
+            for point in clique:
+                if (not m[i][point]):
+                    inClique = False
+                    break
+            if inClique:
+                clique.append(i)
+                added = True
+        if not added:
+            # nouvelle clique avec i tout seul
+            cliques.append([i])
+    return cliques
+
+def plotCliques(points:list, reach:int):
+    cliques = getCliques(points, reach)
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    # colormap that streatches from 0 to len(cliques)
+    colors = plt.get_cmap('hsv', len(cliques))
+    for k in range(len(cliques)):
+        clique = cliques[k]
+        for i in clique:
+            for j in clique:
+                if (i==j):
+                    continue
+                xi = points[i][0]
+                yi = points[i][1]
+                zi = points[i][2]
+                xj = points[j][0]
+                yj = points[j][1]
+                zj = points[j][2]
+                ax.plot([xi, xj], [yi, yj], [zi, zj], color=colors(k))
+    xs = [point[0] for point in points]
+    ys = [point[1] for point in points]
+    zs = [point[2] for point in points]
+    ax.scatter(xs, ys, zs, c='gray')
+
+def getConnectedComponents(points:list, reach:int):
+    m = getAdjMatrix(points, reach)
+    connectedComponents = [[0]]
+    for i in range(1,len(points)):
+        added = False
+        for connectedComponent in connectedComponents:
+            # vérifier si i appartient à la composante connexe
+            inComponent = False
+            for point in connectedComponent:
+                if (m[i][point]):
+                    inComponent = True
+                    break
+            if inComponent:
+                connectedComponent.append(i)
+                added = True
+                break
+        if (not added):
+            connectedComponents.append([i])
+    return connectedComponents
+
+def plotConnectedComponents(points:list, reach:int):
+    m = getAdjMatrix(points, reach)
+    connectedComponents = getConnectedComponents(points, reach)
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    # colormap that streatches from 0 to len(cliques)
+    colors = plt.get_cmap('hsv', len(connectedComponents))
+    for k in range(len(connectedComponents)):
+        clique = connectedComponents[k]
+        for i in clique:
+            for j in clique:
+                if (i==j or not m[i][j]):
+                    continue
+                xi = points[i][0]
+                yi = points[i][1]
+                zi = points[i][2]
+                xj = points[j][0]
+                yj = points[j][1]
+                zj = points[j][2]
+                ax.plot([xi, xj], [yi, yj], [zi, zj], color=colors(k))
+    xs = [point[0] for point in points]
+    ys = [point[1] for point in points]
+    zs = [point[2] for point in points]
+    ax.scatter(xs, ys, zs, c='gray')
+
+points = readCSV("./topology_low.csv")
+
+plotConnectedComponents(points, 20_000)
+plotConnectedComponents(points, 40_000)
+plotConnectedComponents(points, 60_000)
+plt.show()
