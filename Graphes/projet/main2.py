e41b83d08bc0b9d048d104d2298e7fd8f564fa1e
Vianney HERVY on COMPUTAH <vianney.hervy@etu.inp-n7.fr>
2025-01-28 19:56:04 +0100

    maintenant on sauve automatiquement les images dans un dossier. reste plus qu'à les ajouter au latex (pas sûr que ça marche avec le svg)

--- Graphes/projet/main2.py
+++ Graphes/projet/main2.py
@@ -5,7 +5,8 @@ import csv
 import random
 from itertools import combinations
 
-def readCSV(filename: str):
+
+def readCSV(filename: str) -> list[list[float]]:
     with open(filename, newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',')
         datalist = list(spamreader)
@@ -14,11 +15,13 @@ def readCSV(filename: str):
             x = float(row[1])
             y = float(row[2])
             z = float(row[3])
-            points.append([x,y,z])
+            points.append([x, y, z])
     return points
 
-def plotPoints(points: list):
+
+def plotPoints(points: list[list[float]]) -> None:
     fig = plt.figure()
+    fig.suptitle("Positions des satellites")
     ax = fig.add_subplot(projection='3d')
     xs = [point[0] for point in points]
     ys = [point[1] for point in points]
@@ -41,16 +44,18 @@ def getGraph(points: list, reach: int) -> nx.Graph:
             zj = points[j][2]
             dist2 = (xi-xj)**2 + (yi-yj)**2 + (zi-zj)**2
             if dist2 <= reach**2:
-                G.add_edge(i,j)
+                G.add_edge(i, j)
     return G
 
-def plotGraph(G : nx.Graph):
-    pos=nx.get_node_attributes(G, 'pos')
+
+def plotGraph(G: nx.Graph) -> None:
+    pos = nx.get_node_attributes(G, 'pos')
     node_xyz = np.array([pos[n] for n in G.nodes()])
-    edge_xyz = np.array([(pos[u], pos[v]) for u,v in G.edges()])
+    edge_xyz = np.array([(pos[u], pos[v]) for u, v in G.edges()])
 
     # Create the 3D figure
     fig = plt.figure()
+    fig.suptitle("Graphe des satellites")
     ax = fig.add_subplot(projection="3d")
     ax.scatter(*node_xyz.T)
 
@@ -59,59 +64,73 @@ def plotGraph(G : nx.Graph):
         ax.plot(*vizedge.T, color="tab:gray")
 
 
-def getDegrees(G : nx.Graph) -> list:
-    degreesPerNode=list(G.degree([i for i in range(len(G.nodes))]))
-    return [v for _,v in degreesPerNode]
+def getDegrees(G: nx.Graph) -> list:
+    degreesPerNode = list(G.degree([i for i in range(len(G.nodes))]))
+    return [v for _, v in degreesPerNode]
 
-def getAvgDegree(G : nx.Graph) -> int:
+
+def getAvgDegree(G: nx.Graph) -> int:
     degrees = getDegrees(G)
     return sum(degrees) / len(degrees)
 
-def plotDegrees(G : nx.Graph):
+
+def plotDegrees(G: nx.Graph) -> None:
     degrees = getDegrees(G)
     fig = plt.figure()
+    fig.suptitle("Degrés des satellites")
     ax = fig.add_subplot(projection='3d')
     xs = [point[0] for point in points]
     ys = [point[1] for point in points]
     zs = [point[2] for point in points]
     ax.scatter(xs, ys, zs, c=degrees, cmap='jet')
 
-def plotDegreeHist(G : nx.Graph):
+
+def plotDegreeHist(G: nx.Graph) -> None:
     degrees = getDegrees(G)
     n_bins = 10
-    plt.figure()
+    fig = plt.figure()
+    fig.suptitle("Distribution du degré")
     plt.hist(degrees, bins=n_bins)
 
-def getLocalClusteringDegrees(G : nx.Graph) -> list:
+
+def getLocalClusteringDegrees(G: nx.Graph) -> list:
     d = getDegrees(G)
     numTriangles = nx.triangles(G)
     return [numTriangles[i] / (d[i]*(d[i]-1) / 2) if d[i] > 1 else 0 for i in range(len(G.nodes()))]
 
-def plotClusteringDegrees(G : nx.Graph):
+
+def plotClusteringDegrees(G: nx.Graph) -> None:
     clustDeg = getLocalClusteringDegrees(G)
     fig = plt.figure()
+    fig.suptitle("Degrés de clustering des satellites")
     ax = fig.add_subplot(projection='3d')
     xs = [point[0] for point in points]
     ys = [point[1] for point in points]
     zs = [point[2] for point in points]
     ax.scatter(xs, ys, zs, c=clustDeg, cmap='jet')
 
-def plotClusteringDegreeHist(G : nx.Graph):
+
+def plotClusteringDegreeHist(G: nx.Graph) -> None:
     clusteringDegrees = getLocalClusteringDegrees(G)
     n_bins = 10
-    plt.figure()
+    fig = plt.figure()
+    fig.suptitle("Distribution du degré de clustering")
     plt.hist(clusteringDegrees, bins=n_bins)
 
-def getCliques(G : nx.Graph) -> list:
+
+def getCliques(G: nx.Graph) -> list:
     return list(nx.find_cliques(G))
 
-def getNumberCliques(G : nx.Graph) -> int:
+
+def getNumberCliques(G: nx.Graph) -> int:
     return len(getCliques(G))
 
-def plotCliques(G : nx.Graph):
-    pos=nx.get_node_attributes(G, 'pos')
+
+def plotCliques(G: nx.Graph) -> None:
+    pos = nx.get_node_attributes(G, 'pos')
     cliques = getCliques(G)
     fig = plt.figure()
+    fig.suptitle("Cliques de satellites")
     ax = fig.add_subplot(projection='3d')
     nbCliques = getNumberCliques(G)
     # colormap that streatches from 0 to len(cliques)
@@ -127,7 +146,8 @@ def plotCliques(G : nx.Graph):
             ax.plot(*edge_xyz.T, color=colorsRandom[k])
         else:
             combinaison = combinations(clique, 2)
-            edge_xyz = np.array([(pos[u], pos[v]) for u,v in combinaison  if (u,v) in G.edges()])
+            edge_xyz = np.array([(pos[u], pos[v])
+                                for u, v in combinaison if (u, v) in G.edges()])
             for vizedge in edge_xyz:
                 ax.plot(*vizedge.T, color=colorsRandom[k])
     xs = [point[0] for point in points]
@@ -135,46 +155,56 @@ def plotCliques(G : nx.Graph):
     zs = [point[2] for point in points]
     ax.scatter(xs, ys, zs, c='gray')
 
-def getConnectedComponents(G : nx.Graph) -> list:
+
+def getConnectedComponents(G: nx.Graph) -> list:
     return list(nx.connected_components(G))
 
-def getNumberConnectedComponents(G : nx.Graph) -> int:
+
+def getNumberConnectedComponents(G: nx.Graph) -> int:
     return nx.number_connected_components(G)
 
-def plotConnectedComponents(G : nx.Graph):
-    pos=nx.get_node_attributes(G, 'pos')
+
+def plotConnectedComponents(G: nx.Graph) -> None:
+    pos = nx.get_node_attributes(G, 'pos')
     connectedComponents = getConnectedComponents(G)
     fig = plt.figure()
+    fig.suptitle("Composantes connexes de satellites")
     ax = fig.add_subplot(projection='3d')
 
     # colormap that streatches from 0 to len(cliques)
     colors = plt.get_cmap('hsv', getNumberConnectedComponents(G))
-    k=0
+    k = 0
     for component in connectedComponents:
         k += 1
         if len(component) == 1:
             continue
         else:
             combinaison = combinations(component, 2)
-            edge_xyz = np.array([(pos[u], pos[v]) for u,v in combinaison if (u,v) in G.edges()])
+            edge_xyz = np.array([(pos[u], pos[v])
+                                for u, v in combinaison if (u, v) in G.edges()])
             for vizedge in edge_xyz:
                 ax.plot(*vizedge.T, color=colors(k))
-        
+
     xs = [point[0] for point in points]
     ys = [point[1] for point in points]
     zs = [point[2] for point in points]
     ax.scatter(xs, ys, zs, c='gray')
 
-# un dictionnaire dont les cles est le point d'origine et l'élément est un dictionnaire dont les cles sont les points d'arrivés et la valeur est le plus court chemin entre ces deux points.
-def getShortestPath(G : nx.Graph) -> dict:
+
+def getShortestPath(G: nx.Graph) -> dict:
+    """Un dictionnaire dont les cles est le point d'origine et l'élément est un dictionnaire dont les cles sont les points d'arrivés et la valeur est le plus court chemin entre ces deux points.
+    """
     return dict(nx.all_pairs_shortest_path(G))
 
-# un dictionnaire dont les cles est le point d'origine et l'élément est un dictionnaire dont les cles sont les points d'arrivés et la valeur est la longueur entre chaque couple de points
-def getLengthShortestPathDict(G : nx.Graph) -> dict:
+
+def getLengthShortestPathDict(G: nx.Graph) -> dict:
+    """Un dictionnaire dont les cles est le point d'origine et l'élément est un dictionnaire dont les cles sont les points d'arrivés et la valeur est la longueur entre chaque couple de points
+    """
     return dict(nx.all_pairs_shortest_path_length(G))
 
-# une liste avec l'ensemble des longueurs des plus courts chemins
-def getLengthShortestPathList(G : nx.Graph) -> list:
+
+def getLengthShortestPathList(G: nx.Graph) -> list:
+    """Une liste avec l'ensemble des longueurs des plus courts chemins"""
     shortestPathLength = getLengthShortestPathDict(G)
     listShortestPathLength = []
     for _, dict in shortestPathLength.items():
@@ -182,43 +212,77 @@ def getLengthShortestPathList(G : nx.Graph) -> list:
             listShortestPathLength.append(length)
     return listShortestPathLength
 
-def plotShortestPathHist(G : nx.Graph):
+
+def plotShortestPathHist(G: nx.Graph) -> None:
     listShortestPathLength = getLengthShortestPathList(G)
     n_bins = 10
-    plt.figure()
+    fig = plt.figure()
+    fig.suptitle("Distribution des longueurs des plus courts chemins")
     plt.hist(listShortestPathLength, bins=n_bins)
 
-def getNumberShortestPath(G : nx.Graph) -> int:
+
+def getNumberShortestPath(G: nx.Graph) -> int:
     return len(getLengthShortestPathList(G))
 
-def plotShortestPath(G : nx.Graph):
-    pos=nx.get_node_attributes(G, 'pos')
+
+def plotShortestPath(G: nx.Graph) -> None:
+    pos = nx.get_node_attributes(G, 'pos')
     shortestPath = getShortestPath(G)
     fig = plt.figure()
+    fig.suptitle("Plus courts chemins de satellites")
     ax = fig.add_subplot(projection='3d')
 
-    maxLength =  max(getLengthShortestPathList(G))
+    maxLength = max(getLengthShortestPathList(G))
     colors = plt.get_cmap('hsv', maxLength)
-    for _,dict in shortestPath.items():
+    for _, dict in shortestPath.items():
         for _, path in dict.items():
             length = len(path)
             if length <= 1:
                 continue
             else:
-                edge = np.array([(pos[path[k]], pos[path[k+1]]) for k in range(length-1)])
+                edge = np.array([(pos[path[k]], pos[path[k+1]])
+                                for k in range(length-1)])
                 for vizedge in edge:
-                    ax.plot(*vizedge.T, color = colors(length))
+                    ax.plot(*vizedge.T, color=colors(length))
     xs = [point[0] for point in points]
     ys = [point[1] for point in points]
     zs = [point[2] for point in points]
     ax.scatter(xs, ys, zs, c='gray')
 
-points = readCSV("./topology_low.csv")
-G = getGraph(points, 20_000)
+
+DATA_FOLDER: str = "./data/"
+IMG_FOLDER: str = "./doc/img/"
+
+REACHES: list[int] = [20_000, 40_000, 60_000]  # en km
+DENSITIES: list[str] = ["low", "avg", "high"]
+
+FUNCTIONS: list[callable, str] = [[plotGraph, "graph"],
+                                  [plotDegrees, "degrees"],
+                                  [plotDegreeHist, "degree-dist"],
+                                  [plotClusteringDegrees, "clustering-degrees"],
+                                  [plotClusteringDegreeHist,
+                                      "clustering-degree-hist"],
+                                  [plotCliques, "cliques"],
+                                  [plotConnectedComponents,
+                                      "connected-domponents"],
+                                  [plotShortestPath, "shortest-path"]]
 
 
-#print(getShortestPath(G)[2])
+def main():
+    for density in DENSITIES:
+        print(f"Processing density {density}")
+        filename: str = f"{DATA_FOLDER}topology_{density}.csv"
+        points = readCSV(filename)
+        for reach in REACHES:
+            path: str = f"{IMG_FOLDER}/{density}"
+            G: nx.Graph = getGraph(points, reach)
+            for function, name in FUNCTIONS:
+                function(G)
+                plt.savefig(f"{path}/{name}_{reach}.svg")
+                plt.close()
+        print(f"Done processing density {density}")
+    print("Done processing all densities")
 
-plotShortestPath(G)
-#plotConnectedCompotents(points, 20_000)
-plt.show()
+
+if __name__ == "__main__":
+    main()
