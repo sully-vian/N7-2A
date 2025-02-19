6d3745da2889471de816d249afdf86b2bdeb2291
hervyv <vianney.hervy@etu.inp-n7.fr>
2025-02-19 09:50:18 +0100

    ProgAv-TP03: pafini mais flemme idem

--- ProgAv/TP03/README.md
+++ ProgAv/TP03/README.md
@@ -6,3 +6,12 @@
 2. Le proxy utilisé est un `java.util.Collections$UnmodifiableList`. Cette classe statique est une implémentattion de `List` qui lève une exception lorsqu'on tente de modifier la liste.
 
 ## Exercice 2: Proxy et introspection
+
+1. voir code mais bizarre l'exercice un peu
+2. flem
+3. flem
+4. flem
+
+## Exercice 3: Proxy en Python
+
+1.Le programme [exemple_introspection.py](exemple_introspection.py) 
--- ProgAv/TP03/exemple_introspection.py
+++ ProgAv/TP03/exemple_introspection.py
@@ -3,16 +3,16 @@ class C:
 
 c = C()
 c.x = 5
-assert c.x == 5
-assert hasattr(c, 'x')
-assert getattr(c, 'x') == 5
-assert not hasattr(c, "y")
-setattr(c, 'y', 7)
-assert hasattr(c, "y")
-assert getattr(c, 'y') == 7
-assert c.y == 7
-assert vars(c) == {'x': 5, 'y': 7}
-delattr(c, 'x')
-assert not hasattr(c, 'x')
-assert vars(c) == {'y': 7}
+assert c.x == 5 # c.x vaut 5
+assert hasattr(c, 'x') # c a un attribut x
+assert getattr(c, 'x') == 5 # getattr(c, 'x') renvoie 5
+assert not hasattr(c, "y") # c n'a pas d'attribut y
+setattr(c, 'y', 7) # on ajoute un attribut y à c
+assert hasattr(c, "y") # c a maintenant un attribut y
+assert getattr(c, 'y') == 7 # getattr(c, 'y') renvoie 7
+assert c.y == 7 # c.y vaut 7
+assert vars(c) == {'x': 5, 'y': 7} # les attributs de c sont x=5 et y=7
+delattr(c, 'x') # on supprime l'attribut x de c
+assert not hasattr(c, 'x') # c n'a plus d'attribut x
+assert vars(c) == {'y': 7} # l'attribut de c est y=7
 print(dir(c))
