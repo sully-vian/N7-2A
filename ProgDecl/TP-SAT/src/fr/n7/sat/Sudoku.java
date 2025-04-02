b677bc59ec762a5c371c180840ba327cfa178602
hervyv <vianney.hervy@etu.inp-n7.fr>
2025-04-02 16:05:11 +0200

    ProgDecl-TP-SAT: pafini, faut voir forbidLasSol encore

--- ProgDecl/TP-SAT/src/fr/n7/sat/Sudoku.java
+++ ProgDecl/TP-SAT/src/fr/n7/sat/Sudoku.java
@@ -37,7 +37,15 @@ class Sudoku {
      * at least one value.
      */
     private void addExistenceConstraints() {
-        // TODO : to be implemented!
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                BoolExpr formula = context.mkFalse();
+                for (int k = 0; k < grid.length; k++) {
+                    formula = context.mkOr(grid[i][j][k], formula);
+                }
+                solver.add(formula);
+            }
+        }
     }
 
     /**
@@ -45,7 +53,21 @@ class Sudoku {
      * appears exactly one time in each column.
      */
     private void addColumnConstraints() {
-        // TODO : to be implemented!
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                for (int k = 0; k < grid.length; k++) {
+                    BoolExpr andFormula = context.mkTrue();
+                    for (int i2 = 0; i2 < grid.length; i2++) {
+                        if (i2 != i) {
+                            BoolExpr notGrid = context.mkNot(grid[i2][j][k]);
+                            andFormula = context.mkAnd(notGrid, andFormula);
+                        }
+                    }
+                    BoolExpr impliesFormula = context.mkImplies(grid[i][j][k], andFormula);
+                    solver.add(impliesFormula);
+                }
+            }
+        }
     }
 
     /**
@@ -53,7 +75,21 @@ class Sudoku {
      * appears exactly one time in each row.
      */
     private void addRowConstraints() {
-        // TODO : to be implemented!
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                for (int k = 0; k < grid.length; k++) {
+                    BoolExpr andFormula = context.mkTrue();
+                    for (int j2 = 0; j2 < grid.length; j2++) {
+                        if (j != j2) {
+                            BoolExpr notGrid = context.mkNot(grid[i][j2][k]);
+                            andFormula = context.mkAnd(notGrid, andFormula);
+                        }
+                    }
+                    BoolExpr impliesFormula = context.mkImplies(grid[i][j][k], andFormula);
+                    solver.add(impliesFormula);
+                }
+            }
+        }
     }
 
     /**
@@ -61,7 +97,54 @@ class Sudoku {
      * appears exactly one time in each subgrid.
      */
     private void addSubGridsConstraints() {
-        // TODO : to be implemented!
+
+        // itérer sur les sous-grilles
+        for (int bigI = 0; bigI < nInit; bigI++) {
+            for (int bigJ = 0; bigJ < nInit; bigJ++) {
+
+                // itérer sur les cases au sein d'une sous-grille
+                for (int i = bigI * nInit; i < (bigI + 1) * nInit; i++) {
+                    for (int j = bigJ * nInit; j < (bigJ + 1) * nInit; j++) {
+                        for (int k = 0; k < grid.length; k++) {
+                            BoolExpr andFormula = context.mkTrue();
+
+                            // itérer sur les autres cases de la sous-grille
+                            for (int i2 = bigI * nInit; i2 < (bigI + 1) * nInit; i2++) {
+                                for (int j2 = bigJ * nInit; j2 < (bigJ + 1) * nInit; j2++) {
+                                    if ((i2 != i) || (j2 != j)) { // si sur autre case que courante
+                                        BoolExpr notGrid = context.mkNot(grid[i2][j2][k]);
+                                        andFormula = context.mkAnd(notGrid, andFormula);
+                                    }
+                                }
+                            }
+
+                            BoolExpr impliesFormula = context.mkImplies(grid[i][j][k], andFormula);
+                            solver.add(impliesFormula);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void forbidLastSol() {
+        System.out.println("forbidding last sol");
+        Model m = solver.getModel();
+
+        BoolExpr formula = context.mkFalse();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                for (int k = 0; k < grid.length; k++) {
+
+                    if (m.getConstInterp(grid[i][j][k]) != null &&
+                            m.getConstInterp(grid[i][j][k]).isTrue()) {
+                        BoolExpr notGrid = context.mkNot(grid[i][j][k]);
+                        formula = context.mkOr(notGrid, formula);
+                    }
+                }
+            }
+        }
+        solver.add(formula);
     }
 
     /**
@@ -227,10 +310,19 @@ class Sudoku {
      *
      * make run-sudoku SUDOKU_FILE=file_to_use
      */
-    public static void main(String[] args) throws OutOfBoundsException, IOException {
+    public static void main(String[] args) throws OutOfBoundsException, IOException, InterruptedException {
         Sudoku sudoku = Sudoku.loadSudoku(args[0]);
         InputStreamReader aux = new InputStreamReader(System.in);
         BufferedReader in = new BufferedReader(aux);
+
+        int numSol = 0;
+        while (sudoku.solve() == Status.SATISFIABLE) {
+            numSol++;
+            System.out.println("Solution " + numSol + " found!\n");
+            sudoku.print();
+            sudoku.forbidLastSol();
+            // Thread.sleep(500);
+        }
 
         if (sudoku.solve() == Status.SATISFIABLE) {
             System.out.println("Solution found!\n");
