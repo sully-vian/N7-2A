cfec8e9be93ee308ec3b197dee378bc3345d64d4
Vianney HERVY <vian.hervy@free.fr>
2025-04-04 12:57:14 +0200

    ProgDecl-projet: en fait il faut pas exécuter les moves, juste les afficher

--- ProgDecl/projet/cube-abu4991-vhy9665.pl
+++ ProgDecl/projet/cube-abu4991-vhy9665.pl
@@ -32,9 +32,21 @@ put_on_pre(A, B) :-
 put_on_post(A, B) :-
     on(A, B). % A sur B
 
-%%%%%%%%%%%%%%%%
-% Question 4-5 %
-%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%
+% Question 4 %
+%%%%%%%%%%%%%%
+
+:- dynamic(move/2).
+put_on_4(A, B) :-
+    put_on_pre(A, B),
+    retract(on(A,_)),
+    assertz(on(A, B)),
+    assertz(move(A, B)),
+    put_on_post(A, B).
+
+%%%%%%%%%%%%%%
+% Question 5 %
+%%%%%%%%%%%%%%
 
 :- dynamic(move/2).
 put_on(A, B) :-
@@ -51,18 +63,21 @@ put_on(A, B) :-
 %?- put_on(a,table).
 % yes
 
-% ?- put_on(c,a)
+% ?- put_on(c,a).
 % no
+% impossible car c n'est pas libre (on(b,c))
 
-% ?- put_on(b,table), put_on(c,a)
+% ?- put_on(b,table), put_on(c,a).
 % true ?
 % yes
+% en libérant c, on peut mettre c sur a
 
 %%%%%%%%%%%%%%
 % Question 7 %
 %%%%%%%%%%%%%%
 
 % clear/1
+clear(table). % rien à faire pur clear la table
 clear(A) :- % clear A quand rien dessus: izi
     \+ on(_, A).
 clear(A) :- % clear A quand B dessus: récursif
@@ -82,15 +97,51 @@ r_put_on(A,B) :- % clear les deux avant de mettre A sur B
 % Question 8 %
 %%%%%%%%%%%%%%
 
+%%%%%%%
+% (a) %
+%%%%%%%
+
 % achieve_naive/1
 % prend en paramètre une liste de termes représentant des buts à atteindre et
 % affiche les actions nécessaires à la satisfaction de ces buts.
 % ex: achieve([on(a,c), on(c,b)])
-achieve_naive([]). % rien à faire
-achieve_naive([H | T]) :-
+achieve_naive(Goals) :-
+    prepare_achieve(OnEnv, MoveEnv), % on sauvegarde et prépare l'environnement
+    achieve_naive_aux(Goals), % on réalise les buts
+    !, % on stop le backtracking ici
+    listing(move), % on affiche les moves
+    restore_achieve(OnEnv, MoveEnv). % on restore l'environnement
+
+% achieve_naive_aux/1
+% réalise les buts de la liste Goals dans l'ordre où ils sont donnés.
+achieve_naive_aux([]). % rien à faire
+achieve_naive_aux([H | T]) :-
     H = on(A, B),
     r_put_on(A, B),
-    achieve_naive(T).
+    achieve_naive_aux(T).
+
+% prepare_achieve/2
+% enregistre les ons et moves et supprime les moves
+prepare_achieve(OnEnv, MoveEnv) :-
+    findall(on(X,Y), on(X,Y), OnEnv), % on enregistre les ons
+    findall(move(X,Y), move(X,Y), MoveEnv), % on enregistre les moves
+    retractall(move(_, _)). % on enlève tous les moves d'avant
+
+% restore_achieve/2
+restore_achieve(OnEnv, MoveEnv) :-
+    retractall(on(_, _)), % on enlève tous les ons
+    assert_all(OnEnv), % on remet les ons d'origine
+    retractall(move(_, _)), % on enlève tous les moves (ceux ajoutés par achieve)
+    assert_all(MoveEnv). % on remet les moves d'origine
+
+assert_all([]).
+assert_all([H | T]) :-
+    assertz(H),
+    assert_all(T).
+
+%%%%%%%
+% (b) %
+%%%%%%%
 
 % l'approche naive ne fonctionne pas car l'appel récursif peut invalider les
 % buts précédents.
@@ -98,6 +149,10 @@ achieve_naive([H | T]) :-
 % d'invalider on(a,c).
 % Il faut donc trier les buts pour réaliser ceux qui sont plus "bas" en premier.
 % ex: achieve([on(a,c), on(c,b)]) -> achieve([on(c,b), on(a,c)])
+
+%%%%%%%
+% (c) %
+%%%%%%%
 
 % Je peux décrire la liste voulue comme contenant les mêmes éléments que la
 % liste d'origine mais telle qu'aucun élément suivant un on(_,b) ne soit un
@@ -128,9 +183,11 @@ sort_list([], []). % liste vide, rien à trier
 sort_list([H | T], Out) :-
     In = [H | T], % In est la liste d'origine
     permutation(In, Out), % Out est une permutation de In
-    right_order(Out). % vérifier que Out est dans le bon ordre
+    right_order(Out), % vérifier que Out est dans le bon ordre
+    !. % stop le backtracking ici
 
 % achieve/1
-achieve(L) :- % L est la liste de buts
-    sort_list(L, Sorted), % trier la liste
-    achieve_naive(Sorted). % réaliser les buts dans l'ordre
+achieve(Goals) :-
+    listing(on),
+    sort_list(Goals, SortedGoals), % trier la liste
+    achieve_naive(SortedGoals).
