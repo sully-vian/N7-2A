0dac71ec7cbc85e80b08dfdfebb5018937472d84
Vianney HERVY <vian.hervy@free.fr>
2025-04-11 23:40:55 +0200

    ProgDecl-miniprojet: finitoooo (sauf la dernière, mais flemme, je veux dodo aussi).

--- ProgDecl/miniprojet/main.pl
+++ ProgDecl/miniprojet/main.pl
@@ -1,7 +1,8 @@
+% 2SNL Informatique - PDL/PPC
 % Vianney Hervy - vhy9665
 % 2SN - L2
 
-:- initialization(['libtp2.pl']).
+:- include(libtp2).
 
 %%%%%%%%%%%%%%%%%%
 % Modèle basique %
@@ -11,33 +12,37 @@
 % 1 %
 %%%%%
 
-% squareInside/4
-% vrai si le petit carré est dans le grand
-squareInside(T, Ti, Xi, Yi) :-
-    0 #=< Xi, % pas trop à gauche
-    0 #=< Yi, % pas trop bas
-    Xi + Ti #=< T, % pas trop à droite
-    Yi + Ti #=< T. % pas trop haut
-
-% squaresInside/4
-% vrai si tous les petits carrés sont dans le grand
-squaresInside(_, [], [], []). % ok si aucun carré
-squaresInside(T, [Ti|Tq], [Xi|Xq], [Yi|Yq]) :-
-    squareInside(T, Ti, Xi, Yi),
-    squaresInside(T, Tq, Xq, Yq). % appel récursif
+% domains/4
+% vrai si les Xi et Yi sont dans leurs domaines respectifs
+domains([], [], [], _). % ok si aucune variable
+domains([Xi|Xq], [Yi|Yq], [Ti|Tq], T) :-
+    Maxi is T - Ti, % borne supérieure en i
+    fd_domain(Xi, 0, Maxi), % domaine de Xi
+    fd_domain(Yi, 0, Maxi), % domaine de Yi
+    domains(Xq, Yq, Tq, T). % appel récursif
 
 % notColliding/6
-% vrai si les deux carrés ne se recouvrent pas
+% vrai si les carrés i et j ne se recouvrent pas
 notColliding(Ti, Xi, Yi, Tj, Xj, Yj) :-
-    (Xi + Ti #=< Xj) #\/ (Xj + Tj #=< Xi) #\/ (Yi + Ti #=< Yj) #\/ (Yj + Tj #=< Yi).
+    (Xi + Ti #=< Xj) #\/
+    (Xj + Tj #=< Xi) #\/
+    (Yi + Ti #=< Yj) #\/
+    (Yj + Tj #=< Yi).
 
 % nextsNotColliding/6
 % vrai si tous les suivants ne recouvrent pas le carré courant
 nextsNotColliding(_, _, _, [], [], []). % vrai si plus de carrés
 nextsNotColliding(Ti, Xi, Yi, [Tj|Tq], [Xj|Xq], [Yj|Yq]) :-
     notColliding(Ti, Xi, Yi, Tj, Xj, Yj), % i et j ne se recouvrent pas
-    nextsNotColliding(Ti, Xi, Yi, Tq, Xq, Yq), % appel récursif avec i
-    nextsNotColliding(Tj, Xj, Yj, Tq, Xq, Yq). % appel récursif avec j
+    nextsNotColliding(Ti, Xi, Yi, Tq, Xq, Yq). % i ne recouvre aucun suivant de j
+
+% NoneColliding/3
+% vrai si tous les carrés ne se recouvrent pas
+noneColliding([], [], []). % vrai si aucune variable
+noneColliding([Ti|Tq], [Xi|Xq], [Yi|Yq]) :-
+    % tous les carrés suivants ne recouvrent pas le premier
+    nextsNotColliding(Ti, Xi, Yi, Tq, Xq, Yq),
+    noneColliding(Tq, Xq, Yq). % appel récursif
 
 % solve/4
 % vrai si la solution est trouvée
@@ -46,25 +51,28 @@ nextsNotColliding(Ti, Xi, Yi, [Tj|Tq], [Xj|Xq], [Yj|Yq]) :-
 % Ys (out): liste des ordonnées des petits carrés
 % B (out): nombre de backtracks
 solve1(Num, Xs, Ys, B) :-
-    % déclaration des variables
     data(Num, T, Ts), % extraire les données de l'instance
+
+    % déclaration des variables
     length(Ts, N), % N <- len(Ts)
     length(Xs, N), % Xs est liste de taille N
     length(Ys, N), % Ys est liste de taille N
-    fd_domain(Xs, 0, T),
-    fd_domain(Ys, 0, T),
-    % premiers elt de chaque liste
-    [T1|Tq] = Ts,
-    [X1|Xq] = Xs,
-    [Y1|Yq] = Ys,
+    domains(Xs, Ys, Ts, T), % domaines de Xs et Ys
 
     % contraintes
-    squaresInside(T, Ts, Xs, Ys),
-    nextsNotColliding(T1, X1, Y1, Tq, Xq, Yq),
+    noneColliding(Ts, Xs, Ys),
 
     % recherche de solution
     append(Xs, Ys, Vars),
     fd_labeling(Vars, [backtracks(B)]).
+
+%%%%%
+% 2 %
+%%%%%
+
+% J'obtiens la solution suivante:
+% Xs = [0,0,1,2,2,2]
+% Ys = [0,2,2,0,1,2]
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Contraintes redondantes %
@@ -79,37 +87,32 @@ sliceSum(I, [Ti | Tq], [Zi | Zq], S) :-
     sliceSum(I, Tq, Zq, S1), % somme des autres
     S #= S1 + B * Ti. % somme courante
 
-% redondance/5
-redondance(T, _, _, _, T).
-redondance(I, Ts, Xs, Ys, T) :-
+% redondance/4
+% vrai si sliceSum est vrai pour toutes les verticales / horizontales (en
+% fonction de l'appel avec Xs ou Ys)
+redondance(T, _, _, T).
+redondance(I, Ts, Zs, T) :-
     % I est l'indice de la verticale / de l'horizontale
-    sliceSum(I, Ts, Xs, T), % vérif sur la verticale
-    sliceSum(I, Ts, Ys, T), % vérif sur l'horizontale
+    sliceSum(I, Ts, Zs, T), % vérif de la somme
     % appel récursif sur la verticale / horizontale suivante
-    I1 #= I + 1,
-    redondance(I1, Ts, Xs, Ys, T).
+    I1 is I + 1,
+    redondance(I1, Ts, Zs, T).
 
 % solve2/4
 % Idem que solve1 mais avec des contraintes redondantes
 solve2(Num, Xs, Ys, B) :-
-    % déclaration des variables
     data(Num, T, Ts), % extraire les données de l'instance
+
+    % déclaration des variables
     length(Ts, N), % N <- len(Ts)
     length(Xs, N), % Xs est liste de taille N
     length(Ys, N), % Ys est liste de taille N
-    fd_domain(Xs, 0, T),
-    fd_domain(Ys, 0, T),
-    % premiers elt de chaque liste
-    [T1|Tq] = Ts,
-    [X1|Xq] = Xs,
-    [Y1|Yq] = Ys,
+    domains(Xs, Ys, Ts, T), % domaines de Xs et Ys
 
     % contraintes
-    squaresInside(T, Ts, Xs, Ys),
-    nextsNotColliding(T1, X1, Y1, Tq, Xq, Yq),
-
-    % contraintes redondantes
-    redondance(0, Ts, Xs, Ys, T),
+    noneColliding(Ts, Xs, Ys),
+    redondance(0, Ts, Xs, T), % vérif de l'horizontale
+    redondance(0, Ts, Ys, T), % vérif de la verticale
 
     % recherche de solution
     append(Xs, Ys, Vars),
@@ -138,38 +141,84 @@ solve2(Num, Xs, Ys, B) :-
 % Idem que solve2 mais avec labeling
 % NbSol (out): nombre de solutions trouvées
 solve3(Num, Xs, Ys, B, NbSol) :-
-    % déclaration des variables
     data(Num, T, Ts), % extraire les données de l'instance
+
+    % déclaration des variables
     length(Ts, N), % N <- len(Ts)
     length(Xs, N), % Xs est liste de taille N
     length(Ys, N), % Ys est liste de taille N
-    fd_domain(Xs, 0, T),
-    fd_domain(Ys, 0, T),
-    % premiers elt de chaque liste
-    [T1|Tq] = Ts,
-    [X1|Xq] = Xs,
-    [Y1|Yq] = Ys,
+    domains(Xs, Ys, Ts, T), % domaines de Xs et Ys
 
     % contraintes
-    squaresInside(T, Ts, Xs, Ys),
-    nextsNotColliding(T1, X1, Y1, Tq, Xq, Yq),
-
-    % contraintes redondantes
-    redondance(0, Ts, Xs, Ys, T),
+    noneColliding(Ts, Xs, Ys),
+    redondance(0, Ts, Xs, T), % vérif de l'horizontale
+    redondance(0, Ts, Ys, T), % vérif de la verticale
 
     % recherche de solution
-    labeling(Xs, Ys, assign, minmin, B, NbSol).
+    labeling(Xs, Ys, indomain, minmin, B, NbSol).
 
 %%%%%
 % 2 %
 %%%%%
 
-% Je n'arrive pas à faire fonctionner la deuxième instance, même en augmentant
-% CSTRSZ, LOCALSZ, GLOBALSZ et TRAILSZ.
+% Avec assign, l'instance 2 demande 805 backtracks.
+% Avec indomain, l'instance 2 demande 9038 backtracks.
+% indomain sélectionne arbitrairement les variables dans l'ordre où elles
+% apparaissent dans leur domaine. assign associé à minmin choisit plus
+% intelligemment les variables à assigner. Il en résulte une convergence plus
+% rapide, avec moins de backtracks.
 
 %%%%%%%%%%%%%
 % Symétries %
 %%%%%%%%%%%%%
+
+%%%%%
+% 1 %
+%%%%%
+
+% lexSort/6
+% vrai si les carrés de même taille sont triés lexicographiquement par rapport à
+% leurs coordonnées
+lexSort(_, _, _, [], [], []). % vrai si aucun carré restant
+lexSort(_, _, Ti, _, _, [Tj|_]) :- % vrai si les tailles sont différentes
+    Ti #\= Tj. % pas de même taille
+lexSort(Xi, Yi, Ti, [Xj|Xq], [Yj|Yq], [Tj|Tq]) :-
+    Ti #= Tj, % même taille
+    Xi #=< Xj, % tri par rapport à l'abscisse
+    (Xi #\= Xj #\/ Yi #< Yj), % tri par rapport à l'ordonnée
+    lexSort(Xi, Yi, Ti, Xq, Yq, Tq). % appel récursif
+
+% lexSort/3
+% vrai si tous les carrés de même taille sont triés lexicographiquement par
+% rapport à leurs coordonnées
+lexSort([], [], []). % liste triée si vide
+lexSort([Xi|Xq], [Yi|Yq], [Ti|Tq]) :-
+    lexSort(Xi, Yi, Ti, Xq, Yq, Tq), % vérif de l'ordre avec i
+    lexSort(Xq, Yq,Tq). % appel récursif sur les autres carrés
+
+% solve4/5
+% Idem que solve3 mais avec rupture de la symétrie de permuation
+solve4(Num, Xs, Ys, B, NbSol) :-
+    data(Num, T, Ts), % extraire les données de l'instance
+
+    % déclaration des variables
+    length(Ts, N), % N <- len(Ts)
+    length(Xs, N), % Xs est liste de taille N
+    length(Ys, N), % Ys est liste de taille N
+    domains(Xs, Ys, Ts, T), % domaines de Xs et Ys
+
+    % contraintes
+    noneColliding(Ts, Xs, Ys),
+    redondance(0, Ts, Xs, T), % vérif de l'horizontale
+    redondance(0, Ts, Ys, T), % vérif de la verticale
+    lexSort(Xs, Ys, Ts),
+
+    % recherche de solution
+    labeling(Xs, Ys, indomain, minmin, B, NbSol).
+
+% Sans LexSort, on a 480 solutions à l'instance 1.
+% Avec lexSort, il n'en reste plus que 4 distinctes.
+% Ces 4 distinctes ne sont que des rotations d'une solution unique.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%
 % Fonctions auxiliaires %
@@ -177,15 +226,15 @@ solve3(Num, Xs, Ys, B, NbSol) :-
 
 % pour afficher la solution trouvée
 % gnuplot --persist -e "plot 'tiles.txt' with lines"
-
 % solveAndPrint/2
 % raccourcit imprime la sol dans 'tiles.txt'
 solveAndPrint(Num, SolveNum) :-
     ( SolveNum = 1 -> solve1(Num, Xs, Ys, B)
     ; SolveNum = 2 -> solve2(Num, Xs, Ys, B)
     ; SolveNum = 3 -> solve3(Num, Xs, Ys, B, NbSol)
+    ; SolveNum = 4 -> solve4(Num, Xs, Ys, B, NbSol)
     ),
     data(Num, _, Ts),
     format('Num backtracks: ~w~n', [B]),
-    ( SolveNum = 3 -> format('Number of solutions: ~w~n', [NbSol]) ; true ),
+    ( SolveNum = 3 ; SolveNum = 4 -> format('Number of solutions: ~w~n', [NbSol]) ; true ),
     printsol('tiles.txt', Xs, Ys, Ts).
